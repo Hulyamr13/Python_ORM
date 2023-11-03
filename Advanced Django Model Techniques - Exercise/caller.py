55339f344ec68a72382f26d1cd76a107d949ab9e
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2023-11-03 16:46:14 +0200

    updates

--- Advanced Django Model Techniques - Exercise/caller.py
+++ Advanced Django Model Techniques - Exercise/caller.py
@@ -7,13 +7,9 @@ django.setup()
 
 # Import your models here
 
-# Create queries within functions
-
 from main_app.models import SpiderHero, FlashHero
-
-# Task 4
 # Create instance of SpiderHero
-spiderman = SpiderHero(name="Spider-Man", hero_title="Spider Hero", energy=70)
+spiderman = SpiderHero(name="Spider-Man", hero_title="Spider Hero", energy=100)
 # Create instance of FlashHero
 flash = FlashHero(name="The Flash", hero_title="Flash Hero", energy=70)
 
@@ -27,10 +23,11 @@ print(flash.run_at_super_speed())
 print(spiderman.swing_from_buildings())
 
 # Recharge the energy of Spider-Man and The Flash using the mixin method
-spiderman.recharge_energy(30)
+spiderman.recharge_energy(195)
 flash.recharge_energy(40)
 
 # Now you can check the updated energy levels
 print(f"{spiderman.name} - Energy: {spiderman.energy}")
 print(f"{flash.name} - Energy: {flash.energy}")
+
 
--- Advanced Django Model Techniques - Exercise/main_app/models.py
+++ Advanced Django Model Techniques - Exercise/main_app/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+
 class RechargeEnergyMixin:
     def recharge_energy(self, amount):
         self.energy = min(self.energy + amount, 100)
         self.save()
+        return self.energy
+
 
 class Hero(models.Model, RechargeEnergyMixin):
     name = models.CharField(max_length=100)
@@ -13,29 +16,26 @@ class Hero(models.Model, RechargeEnergyMixin):
     def __str__(self):
         return self.name
 
-class SpiderHero(Hero, RechargeEnergyMixin):
-    SPIDER_HERO_ENERGY_DECREASE = 80
 
+class SpiderHero(Hero):
     def swing_from_buildings(self):
-        if self.energy <= 0:
-            return f"{self.name} as Spider Hero is out of web shooter fluid"
-        else:
-            self.energy -= self.SPIDER_HERO_ENERGY_DECREASE
+        if self.energy >= 80:
+            self.energy -= 80
+            self.save()
             return f"{self.name} as Spider Hero swings from buildings using web shooters"
+        return f"{self.name} as Spider Hero is out of web shooter fluid"
 
     class Meta:
         proxy = True
 
-class FlashHero(Hero, RechargeEnergyMixin):
-    FLASH_HERO_ENERGY_DECREASE = 65
 
+class FlashHero(Hero):
     def run_at_super_speed(self):
-        if self.energy <= 0:
-            return f"{self.name} as Flash Hero needs to recharge the speed force"
-        else:
-            self.energy -= self.FLASH_HERO_ENERGY_DECREASE
+        if self.energy >= 65:
+            self.energy -= 65
             self.save()
             return f"{self.name} as Flash Hero runs at lightning speed, saving the day"
+        return f"{self.name} as Flash Hero needs to recharge the speed force"
 
     class Meta:
         proxy = True
