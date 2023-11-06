7ac5d21454b39bcb37e71def5c3272a3b8b32a05
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2023-11-07 00:54:28 +0200

    updates

--- Advanced Django Model Techniques - Exercise/main_app/models.py
+++ Advanced Django Model Techniques - Exercise/main_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+# Task 4
+
 
 class RechargeEnergyMixin:
     def recharge_energy(self, amount):
         self.energy = min(self.energy + amount, 100)
         self.save()
-        return self.energy
 
 
 class Hero(models.Model, RechargeEnergyMixin):
@@ -13,17 +14,15 @@ class Hero(models.Model, RechargeEnergyMixin):
     hero_title = models.CharField(max_length=100)
     energy = models.PositiveIntegerField()
 
-    def __str__(self):
-        return self.name
-
 
 class SpiderHero(Hero):
     def swing_from_buildings(self):
-        if self.energy >= 80:
-            self.energy -= 80
+        self.energy -= 80
+        if self.energy <= 0:
+            return f"{self.name} as Spider Hero is out of web shooter fluid"
+        else:
             self.save()
             return f"{self.name} as Spider Hero swings from buildings using web shooters"
-        return f"{self.name} as Spider Hero is out of web shooter fluid"
 
     class Meta:
         proxy = True
@@ -31,11 +30,12 @@ class SpiderHero(Hero):
 
 class FlashHero(Hero):
     def run_at_super_speed(self):
-        if self.energy >= 65:
-            self.energy -= 65
+        self.energy -= 65
+        if self.energy <= 0:
+            return f"{self.name} as Flash Hero needs to recharge the speed force"
+        else:
             self.save()
             return f"{self.name} as Flash Hero runs at lightning speed, saving the day"
-        return f"{self.name} as Flash Hero needs to recharge the speed force"
 
     class Meta:
         proxy = True
